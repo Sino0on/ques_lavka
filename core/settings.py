9d34e08ca2cf99bf31da23dcc659e27258932f33
Dastan <dastan.kubanuulu1@gmail.com>
2025-05-25 00:48:59 +0600

    das

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!epamr8=185!uyy!^fvv_+cv1*$o1m#y2(53h379g&#4qdqg#r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
